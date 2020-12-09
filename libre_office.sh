1a8c0d662174ddff26e442b7078e4726d2870a09
Max Wets <max.wets@gmail.com>
2020-12-09 14:45:13 +0100

    libre-office

---
+++ libre_office.sh
@@ -0,0 +1,8 @@
+# Add libre office
+sudo add-apt-repository ppa:libreoffice/ppa -y
+sudo apt update
+sudo apt install libreoffice
+sudo apt install hunspell-en-us hyphen-en-us mythes-en-us libreoffice-help-en-us
+sudo apt install hunspell-fr hyphen-fr mythes-fr libreoffice-help-fr
+sudo apt install libreoffice-gtk3
+sudo apt install libreoffice-style*
